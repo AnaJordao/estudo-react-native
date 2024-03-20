5fad753596086ae0cccfc999728fa5dadc8ee01d
AnaJordao <acjpjvjp@gmail.com>
2024-03-20 14:57:50 -0300

    Deleção feita e modal de criação de goals

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,33 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler(){
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler(){
+    setModalIsVisible(false);
+  }
 
   function addGoalHandler(enteredGoalText){
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals, 
-      {text: enteredGoalText, key: Math.random().toString()}
+      {text: enteredGoalText, id: Math.random().toString()}
     ]);
+    endAddGoalHandler();
   }
 
-  function deleteGoalHandler(){
-    console.log('DELETE')
+  function deleteGoalHandler(id){
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler} />
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler} />;
+          return <GoalItem 
+                  id={itemData.item.id} 
+                  text={itemData.item.text} 
+                  onDeleteItem={deleteGoalHandler} />;
         }} 
-        // keyExtractor={(item, index)=> { // se o atributo já não tiver o nome key, se tiver ele consegue inferir automaticamente
-        //   return item.id;
-        // }}
+        keyExtractor={(item, index)=> { // se o atributo já não tiver o nome key, se tiver ele consegue inferir automaticamente
+          return item.id;
+        }}
         />
       </View>
     </View>
