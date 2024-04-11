76b626d2eeb6b4f2bcccd6c3f8543ca25988520e
AnaJordao <acjpjvjp@gmail.com>
2024-04-11 17:45:24 -0300

    Navegação entre duas telas

---
+++ MealsApp/screens/MealsOverviewScreen.js
@@ -0,0 +1,49 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
+
+function MealsOverviewScreen({ route, navigation }){
+    const catId = route.params.categoryId
+
+    // using useLayoutEffect instead useEffect so the title doesn't delay
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((categgory)=> categgory.id === catId).title;
+        navigation.setOptions({
+            title: categoryTitle
+        });
+    }, [catId, navigation])
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    function renderMealItem(itemData){
+        const item = itemData.item;
+        const mealItemProps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            complexity: item.complexity,
+            affordability: item.affordability,
+            duration: item.duration
+        }
+        return (
+            <MealItem {...mealItemProps}/>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
+        </View>
+    )
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    }
+})
