a00f1df013eff3598126e3f766e7d93dd86545cc
AnaJordao <acjpjvjp@gmail.com>
2024-06-14 15:56:55 -0300

    Sec 12 - Integração com SQLite (bug: não inserindo no banco, sem console.log no terminal)

--- FavoritePlacesApp/screens/AllPlaces.js
+++ FavoritePlacesApp/screens/AllPlaces.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
+import { fetchPlaces } from "../util/database";
 
-function AllPlaces() {
-  return <PlacesList />;
+function AllPlaces({ route }) {
+  const [loadedplaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function loadPlaces() {
+      await fetchPlaces();
+    }
+
+    if (isFocused && route.params) {
+      loadPlaces();
+      // setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused]);
+
+  return <PlacesList places={loadedplaces} />;
 }
 
 export default AllPlaces;
